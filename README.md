95738f9b09e5fc23739bb8db4a2b3b3326911f47
Razzile <comfycallum@gmail.com>
2016-10-29 23:24:26 +0100

    Making a start

---
+++ README.md
@@ -0,0 +1,3 @@
+# An iOS cmake helper
+
+not finished
--- ios-new.cmake
+++ ios-new.cmake
@@ -3,6 +3,15 @@
 # set target archs if not set already
 # custom configuration stuff
 
+set(TOOLCHAIN_ROOT GLOB "$ENV{THEOS}/toolchain/*/iphone")
+
+
+
+# cant find theos toolchain, search for toolchain with xcode
+if (NOT EXISTS ${TOOLCHAIN_ROOT})
+    
+endif()
+
 if (NOT DEFINED ARCH)
     set(IOS_ARCH armv7 arm64)
 endif()
